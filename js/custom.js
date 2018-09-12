d64d5cb5da2442a9f44e9fa6e8d5029814f4de7e
Jacob Bøtter <jacob@boetter.dk>
2018-09-12 12:44:34 +0200

    arh shit

--- js/custom.js
+++ js/custom.js
@@ -29,7 +29,7 @@ ui.createComponent('product', {
 
       styles: {
         product: {
-          'text-align': 'left' },
+          'text-align': 'center' },
 
         button: {
           'font-family': 'Montserrat,sans-serif',
@@ -39,11 +39,14 @@ ui.createComponent('product', {
           'border-radius': '4px',
           'padding': '16px 24px 17px',
           'margin': '0 0 10px 0!important',
-          'box-shadow': '0 1px 4px rgba(0,0,0,.1),0 2px 4px rgba(0,0,0,.1)',
+          'box-shadow': '0 1px 4px rgba(0,0,0,.1),0 2px 4px rgba(0,0,0,.1)'
 
           ':hover': {
-            'background-color': '#3498db',
-            'box-shadow': '3px 3px 0 #2980b9' },
+            'opacity': '1',
+            'box-shadow': '0 6px 7px 0 rgba(0, 0, 0, 0.15), 0 0px 5px 0px rgba(0, 0, 0, 0.1)',
+            '-webkit-transform': 'translateY(-2px) !important',
+            'transform': 'translateY(-2px) !important',
+            'transition': '0.35s !important' },
 
           ':focus': {
             'background-color': '#3498db',
