2981eb581123e960423b78e71ac508802b795a5b
Jacob Bøtter <jacob@boetter.dk>
2018-09-12 12:34:28 +0200

    unsafe waters here

--- js/custom.js
+++ js/custom.js
@@ -32,11 +32,26 @@ ui.createComponent('product', {
           'text-align': 'left' },
 
         button: {
-          'background-color': '#1abc9c',
-          'border-radius': 0,
-          'padding': '12px 18px',
+          'font-family': 'Nunito,sans-serif',
+          'font-weight': '600',
+          'font-size': '16px',
+          'background-color': '#4caf50',
+          'border-radius': '4px',
+          'padding': '16px 24px 17px',
           'margin': '0 0 10px 0!important',
-          'box-shadow': '3px 3px 0 #16a085',
+          'box-shadow': '0 1px 4px rgba(0,0,0,.1),0 2px 4px rgba(0,0,0,.1)',
+
+          ':before': {
+            'content': '""',
+            'position': 'absolute',
+            'top': '0',
+            'left': '0',
+            'width': '100%',
+            'height': '100%',
+            'background': 'linear-gradient(to bottom,#fff 0,#000 100%)',
+            'z-index': '-1',
+            'opacity': '.1' },
+
           ':hover': {
             'background-color': '#3498db',
             'box-shadow': '3px 3px 0 #2980b9' },
