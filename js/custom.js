fd4316d32a67161f7250feda07aa06c0ee1a0878
Jacob Bøtter <jacob@boetter.dk>
2018-09-12 12:47:28 +0200

    cut the hover

--- js/custom.js
+++ js/custom.js
@@ -32,22 +32,11 @@ ui.createComponent('product', {
           'text-align': 'center' },
 
         button: {
-          'font-family': 'Montserrat,sans-serif',
+          'font-family': 'Helvetica,sans-serif',
           'font-weight': '600',
           'font-size': '16px',
           'background-color': '#4caf50',
           'border-radius': '4px',
           'padding': '16px 24px 17px',
           'margin': '0 0 10px 0!important',
-          'box-shadow': '0 1px 4px rgba(0,0,0,.1),0 2px 4px rgba(0,0,0,.1)',
-
-          ':hover': {
-            'opacity': '1',
-            'box-shadow': '0 6px 7px 0 rgba(0, 0, 0, 0.15), 0 0px 5px 0px rgba(0, 0, 0, 0.1)',
-            '-webkit-transform': 'translateY(-2px) !important',
-            'transform': 'translateY(-2px) !important',
-            'transition': '0.35s !important' },
-
-          ':focus': {
-            'background-color': '#3498db',
-            'box-shadow': '3px 3px 0 #2980b9' } } } } } });
+          'box-shadow': '0 1px 4px rgba(0,0,0,.1),0 2px 4px rgba(0,0,0,.1)' } } } } });
