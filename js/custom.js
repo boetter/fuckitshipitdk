424fc2847a39d7251cacbfd29858da3bdcdc363a
Jacob Bøtter <jacob@boetter.dk>
2018-09-12 12:46:13 +0200

    comma

--- js/custom.js
+++ js/custom.js
@@ -39,7 +39,7 @@ ui.createComponent('product', {
           'border-radius': '4px',
           'padding': '16px 24px 17px',
           'margin': '0 0 10px 0!important',
-          'box-shadow': '0 1px 4px rgba(0,0,0,.1),0 2px 4px rgba(0,0,0,.1)'
+          'box-shadow': '0 1px 4px rgba(0,0,0,.1),0 2px 4px rgba(0,0,0,.1)',
 
           ':hover': {
             'opacity': '1',
